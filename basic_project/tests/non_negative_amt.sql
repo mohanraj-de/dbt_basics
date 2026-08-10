ed1a0c99c2731730c94fcb0145400f50dcdd1b4b
Mohan Raj <mohanrajgurunathann@gmail.com>
2026-08-10 23:45:49 +0530

    dbt seed basics

--- basic_project/tests/non_negative_amt.sql
+++ basic_project/tests/non_negative_amt.sql
@@ -1,3 +1,6 @@
-select * 
-from {{ ref("bronze_fact_sales") }} 
-where gross_amount < 0 and net_amount< 0
+select 
+* 
+from
+{{ ref("bronze_fact_sales") }} 
+where gross_amount < 0 
+and net_amount< 0
